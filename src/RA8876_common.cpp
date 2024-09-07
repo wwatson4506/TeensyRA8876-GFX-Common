404b7baa1e094045e1e622c2bec3254cc183f3bc
Warren Watson <wwatson4506@gmail.com>
2024-09-07 10:02:12 -0700

    Fix DrawPixel(). AGAIN.
    Failed to correctly render OpenSans24 font in 8bit bus mode.
    Changed to:
    void RA8876_common::drawPixel(ru16 x, ru16 y, ru16 color) {
        graphicMode(true);
        setPixelCursor(x, y);
        ramAccessPrepare();
    	if(_bus_width == 16) {
    		lcdDataWrite16(color);
    	} else {
    		lcdDataWrite(color);
    		lcdDataWrite(color >> 8);
        }
    //#if defined(use_lcdDataWrite16bbp)
    //    lcdDataWrite16bbp(color);
    //#endif
    }

--- src/RA8876_common.cpp
+++ src/RA8876_common.cpp
@@ -1198,9 +1198,12 @@ void RA8876_common::drawPixel(ru16 x, ru16 y, ru16 color) {
     graphicMode(true);
     setPixelCursor(x, y);
     ramAccessPrepare();
-//    lcdDataWrite16(color);
-    lcdDataWrite(color);
-    lcdDataWrite(color >> 8);
+	if(_bus_width == 16) {
+		lcdDataWrite16(color);
+	} else {
+		lcdDataWrite(color);
+		lcdDataWrite(color >> 8);
+    }
 //#if defined(use_lcdDataWrite16bbp)
 //    lcdDataWrite16bbp(color);
 //#endif
