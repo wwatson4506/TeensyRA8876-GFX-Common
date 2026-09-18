2194f9fe9cf223f48bb4d5bf60487629e71628d6
Warren Watson <wwatson4506@gmail.com>
2026-09-18 13:26:15 -0700

    Update RA8876_common.cpp

--- src/RA8876_common.cpp
+++ src/RA8876_common.cpp
@@ -5727,6 +5727,150 @@ void RA8876_common::charBounds(char c, int16_t *x, int16_t *y,
     }
 }
 
+// Return the width of a text string
+// - num =  max characters to process, or 0 for entire string (null-terminated)
+uint16_t RA8876_common::measureTextWidth(const char* text, int num) {
+	uint16_t maxH = 0;
+	uint16_t currH = 0;
+	uint16_t n = num;
+
+	if (n == 0) {
+		n = strlen(text);
+	};
+
+	for (int i = 0; i < n; i++) {
+		if (text[i] == '\n') {
+			// For multi-line strings, retain max width
+			if (currH > maxH)
+				maxH = currH;
+			currH = 0;
+		}
+		else {
+			uint16_t h, w;
+			measureChar(text[i], &w, &h);
+			currH += w;
+		}
+	}
+	uint16_t h = maxH > currH ? maxH : currH;
+	return h;
+}
+
+// Return the height of a text string
+// - num =  max characters to process, or 0 for entire string (null-terminated)
+uint16_t RA8876_common::measureTextHeight(const char* text, int num) {
+	int lines = 1;
+	uint16_t n = num;
+	if (n == 0) {
+		n = strlen(text);
+	};
+	for (int i = 0; i < n; i++) {
+		if (text[i] == '\n') {
+			lines++;
+		}
+	}
+	return ((lines - 1) * fontLineSpace() + fontCapHeight());
+}
+
+static inline uint32_t fetchbit(const uint8_t *p, uint32_t index)
+{
+	return (p[index >> 3] & (0x80 >> (index & 7)));
+}
+
+static uint32_t fetchbits_unsigned(const uint8_t *p, uint32_t index, uint32_t required)
+{
+uint32_t val;
+uint8_t *s = (uint8_t *)&p[index>>3];
+
+#ifdef UNALIGNED_IS_SAFE
+	val = *(uint32_t *)s; // read 4 bytes - unaligned is ok
+	val = __builtin_bswap32(val); // change to big-endian order
+#else
+	val = s[0] << 24;
+	val |= (s[1] << 16);
+	val |= (s[2] << 8);
+	val |= s[3];
+#endif
+	val <<= (index & 7); // shift out used bits
+	if (32 - (index & 7) < required) { // need to get more bits
+		val |= (s[4] >> (8 - (index & 7))); 
+        }
+	val >>= (32-required); // right align the bits
+	return val;
+}
+
+static uint32_t fetchbits_signed(const uint8_t *p, uint32_t index, uint32_t required)
+{
+	uint32_t val = fetchbits_unsigned(p, index, required);
+	if (val & (1 << (required - 1))) {
+		return (int32_t)val - (1 << required);
+	}
+	return (int32_t)val;
+}
+
+// Measure the dimensions for a single character
+void RA8876_common::measureChar(unsigned char c, uint16_t* w, uint16_t* h) {
+	// Treat non-breaking space as normal space
+	if (c == 0xa0) {
+		c = ' ';
+	}
+
+	// Is current font a T3 font or the default Adafruit-GFX font?
+	if (font) {
+		// ILI9341_T3 font
+		*h = font->cap_height;
+		*w = 0;
+
+		uint32_t bitoffset;
+		const uint8_t *data;
+
+		if (c >= font->index1_first && c <= font->index1_last) {
+			bitoffset = c - font->index1_first;
+			bitoffset *= font->bits_index;
+		}
+		else if (c >= font->index2_first && c <= font->index2_last) {
+			bitoffset = c - font->index2_first + font->index1_last - font->index1_first + 1;
+			bitoffset *= font->bits_index;
+		}
+		else if (font->unicode) {
+			return; // TODO: implement sparse unicode
+		}
+		else {
+			return;
+		}
+
+		data = font->data + fetchbits_unsigned(font->index, bitoffset, font->bits_index);
+
+		uint32_t encoding = fetchbits_unsigned(data, 0, 3);
+
+		if (encoding != 0) return;
+
+		//uint32_t width =
+		fetchbits_unsigned(data, 3, font->bits_width);
+		bitoffset = font->bits_width + 3;
+
+		//uint32_t height =
+		fetchbits_unsigned(data, bitoffset, font->bits_height);
+		bitoffset += font->bits_height;
+
+		//int32_t xoffset =
+		fetchbits_signed(data, bitoffset, font->bits_xoffset);
+		bitoffset += font->bits_xoffset;
+
+		//int32_t yoffset =
+		fetchbits_signed(data, bitoffset, font->bits_yoffset);
+		bitoffset += font->bits_yoffset;
+
+		uint32_t delta = fetchbits_unsigned(data, bitoffset, font->bits_delta);
+		*w = delta;
+	}
+	else {
+		// Adafruit-GFX default font has fixed 6x8 dimensions
+		*w = 6 * textsize;
+		*h = 8 * textsize;
+	}
+
+}
+
 // Add in Adafruit versions of text bounds calculations.
 void RA8876_common::getTextBounds(const uint8_t *buffer, uint16_t len, int16_t x, int16_t y,
                                   int16_t *x1, int16_t *y1, uint16_t *w, uint16_t *h) {
